f762c129ad63b9983e48d23af6b55d9e2135d098
Helmuth Bohle <helmuthbohle@MacBook-Pro-de-Helmuth.local>
2026-09-23 01:29:50 -0300

    Fix compilación Android TV: banner PNG, estilos AppCompat y Gradle 8.5

--- android/app/src/main/java/com/antenasur/tv/MainActivity.java
+++ android/app/src/main/java/com/antenasur/tv/MainActivity.java
@@ -1,7 +1,7 @@
 package com.antenasur.tv;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
